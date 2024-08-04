495e9cd73ceee597793a97f9af69cb821e9c17ab
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-08-04 18:56:47 +0530

    Improved navbar

--- src/Components/Common/Navbar.tsx
+++ src/Components/Common/Navbar.tsx
@@ -1,3 +1,11 @@
 export default function Navbar() {
-   return <div>Navbar Component</div>
+   return (
+      <div className="h-16 grid grid-cols-[10rem,1fr,10rem] items-center justify-between px-4">
+         <div className="flex gap-4 items-center h-full">
+            <img src="/logo-circle.png" className="h-12 -mt-[0.5]" />
+         </div>
+         <div className="font-medium text-xl text-text1 text-center">Medicinery</div>
+         <div className="flex items-center gap-2 justify-end"></div>
+      </div>
+   )
 }
