f1c27764bdd1970688f69589b76f2d5c5335050e
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-08-04 18:57:13 +0530

    Improved layout

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,20 +1,26 @@
 import { lazy } from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Navbar from "./Common/Navbar"
+import Sidebar from "./Common/Sidebar"
 
 const Chat = lazy(() => import("./Routes/Chat"))
 const Welcome = lazy(() => import("./Routes/Welcome"))
 
 export default function App() {
    return (
-      <div>
+      <div className="bg-bg1 grid grid-rows-[min-content,1fr] h-screen">
          <Navbar />
-         <BrowserRouter>
-            <Routes>
-               <Route path="" element={<Welcome />} />
-               <Route path=":chatID" element={<Chat />} />
-            </Routes>
-         </BrowserRouter>
+         <main className="h-full grid grid-cols-[min-content,1fr]">
+            <Sidebar />
+            <div className="text-text1">
+               <BrowserRouter>
+                  <Routes>
+                     <Route path="" element={<Welcome />} />
+                     <Route path="/:chatID" element={<Chat />} />
+                  </Routes>
+               </BrowserRouter>
+            </div>
+         </main>
       </div>
    )
 }
